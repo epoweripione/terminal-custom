2c758350a8623425df3274e7b34774ec667a5db6
epoweripione <siyuwuxin@gmail.com>
2018-08-16 13:09:59 +0800

    fix how to use

--- zsh/zsh_download_custom_script.sh
+++ zsh/zsh_download_custom_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## How to use
-## source <(curl -sL https://git.io/fNpeJ)
+## source <(curl -sL https://git.io/fNpeJ) && ./zsh_update.sh
 ## or
 ## cd ~ && curl -fSL https://raw.githubusercontent.com/epoweripione/terminal-custom/master/zsh/zsh_download_custom_script.sh -o ~/zsh_download_custom_script.sh && chmod +x ~/zsh_download_custom_script.sh && ./zsh_download_custom_script.sh && ./zsh_update.sh
 
