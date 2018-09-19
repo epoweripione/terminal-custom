174a25ac8213f60a83f8758d98c8a617ddaa4791
epoweripione <siyuwuxin@gmail.com>
2018-09-19 13:16:17 +0800

    more fix for gvm upgrade

--- zsh/zsh_upgrade_all_packages.sh
+++ zsh/zsh_upgrade_all_packages.sh
@@ -92,7 +92,12 @@ fi
 
 if [[ -d "$HOME/.gvm" ]]; then
     colorEcho ${BLUE} "Updating gvm & go..."
-    REMOTE_VERSION=$(proxychains4 curl -s https://golang.org/dl/ | grep -m 1 -o 'go\([0-9]\)\+\.\([0-9]\)\+')
+    if [[ -x "$(command -v proxychains4)" ]]; then
+        REMOTE_VERSION=$(proxychains4 curl -s https://golang.org/dl/ | grep -m 1 -o 'go\([0-9]\)\+\.\([0-9]\)\+')
+    else
+        REMOTE_VERSION=$(curl -s https://golang.org/dl/ | grep -m 1 -o 'go\([0-9]\)\+\.\([0-9]\)\+')
+    fi
+
     if [[ ! "$(gvm list | grep $REMOTE_VERSION)" ]]; then
         if [[ -x "$(command -v proxychains4)" ]]; then
             proxychains4 gvm install $REMOTE_VERSION && gvm use $REMOTE_VERSION --default
