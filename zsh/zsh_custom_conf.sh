ae3342ad24b937110b55d9acf4d688555ac4f7ce
epoweripione <siyuwuxin@gmail.com>
2018-10-09 11:55:33 +0800

    fix for load-nvmrc

--- zsh/zsh_custom_conf.sh
+++ zsh/zsh_custom_conf.sh
@@ -209,7 +209,7 @@ if [[ -d "$HOME/.nvm" ]]; then
       if [[ -n "$nvmrc_path" ]]; then
           local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
 
-          if [[ "$nvmrc_node_version" = "N/A" ]; then
+          if [[ "$nvmrc_node_version" == "N/A" ]]; then
               nvm install
           elif [[ "$nvmrc_node_version" != "$node_version" ]]; then
               nvm use
