a45f30b7c4b07aae5ce611548d2f231cbb054489
epoweripione <siyuwuxin@gmail.com>
2018-12-09 23:00:59 +0800

    fix

--- zsh/zsh_download_custom_script.sh
+++ zsh/zsh_download_custom_script.sh
@@ -56,7 +56,6 @@ chmod +x ~/ssl-cert-info.sh
 chmod +x ~/ssl-check-expire-date.sh
 
 chmod +x ~/conda_python_installer.sh
-# chmod +x ~/cross_gfw_installer.sh
 chmod +x ~/docker_installer.sh
 chmod +x ~/geoip_installer.sh
 chmod +x ~/gvm_go_installer.sh
